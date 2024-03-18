b6a19b51cc7a1d27132267b6eabb315ae55a0568
ValeriyKliuk <valeriy.kliuk@gmail.com>
2024-03-17 23:06:51 -0400

    initial spring-boot commit

---
+++ src/test/kotlin/ca/kliuk/products/ProductsApplicationTests.kt
@@ -0,0 +1,13 @@
+package ca.kliuk.products
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
